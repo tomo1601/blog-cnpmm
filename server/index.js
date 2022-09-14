307c137c21021684a530768ca60a11c78902632a
tomo1601 <quangssoss@gmail.com>
2022-09-14 22:52:35 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+
+const express = require( 'express')
+const mongoose = require('mongoose')
+
+const connectDB = async () =>{
+    try {
+        await mongoose.connect('mongodb+srv://vanquang:1234567890@webblog.ybxxmbt.mongodb.net/?retryWrites=true&w=majority', {
+            
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('MongoDB connected')
+    } catch (error){
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+connectDB()
+
+const app =express()
+
+app.get('/', (rep,res) => res.send('Hello word!'))
+
+const PORT = 5000
+
+app.listen(PORT, () =>console.log(`server start on port ${PORT}`))
