dec4c215882bea85959f30ae92709f7926978ea2
tomo1601 <quangssoss@gmail.com>
2022-11-13 20:57:12 +0700

    update register otp

--- client/src/components/auth/RegisterForm.js
+++ client/src/components/auth/RegisterForm.js
@@ -1,52 +1,84 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useState, useContext } from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
 import AlertMessage from '../layout/AlertMessage'
 
 
-const UserLoginForm =() =>{
+const UserLoginForm = () => {
 
     //context
-    const {registerUser} = useContext(AuthContext)
+    const { registerUser } = useContext(AuthContext)
     const [authloading, setAuthLoading] = useState(false);
     //local state
     const [RegisterForm, setRegisterForm] = useState({
         email: '',
         password: '',
-        username:'',
-        confirmpassword:''
+        username: '',
+        confirmpassword: ''
     })
-   
-    const {email, password, username, confirmpassword} = RegisterForm
-    const onChangeRegisterForm = event => setRegisterForm({
-        ...RegisterForm, [event.target.name]:event.target.value
-    })
-    
-    const [alert, setAlert] =useState(null)
 
-    
-    const userRegister = async event =>{
+    const { email, password, username, confirmpassword } = RegisterForm
+    const onChangeRegisterForm = event => setRegisterForm({
+        ...RegisterForm, [event.target.name]: event.target.value
+    })
+
+    const [Otp, setOtp] = useState({
+        waitingOtp: false,
+        OtpRes:''
+    })
+
+    const {OtpRes} = Otp
+    const onChangeOtp = event => setOtp({
+        ...Otp, OtpRes: event.target.value
+    })
+
+    let acceptOtp
+    if (!Otp.waitingOtp) acceptOtp = (
+        <>
+            <Button className='mt-2' variant='primary' type='submit'>Register</Button>
+        </>
+    )
+    else acceptOtp = (
+        <>
+            <Form.Group>
+                <Form.Label>OTP</Form.Label>
+                <Form.Control
+                    type='text'
+                    placeholder=''
+                    name='otp'
+                    value={OtpRes}
+                    required
+                    onChange={onChangeOtp}
+                />
+            </Form.Group>
+            <Button className='mt-2' variant='primary' type='submit'>Register</Button>
+        </>
+    )
+    const [alert, setAlert] = useState(null)
+
+
+    const userRegister = async event => {
         setAuthLoading(true);
         event.preventDefault()
-        if (confirmpassword!==password){
-            setAlert({type: 'danger', message: 'You must re-enter the correct confirmation password'})
-                setTimeout(()=> setAlert(null), 5000)
+        if (confirmpassword !== password) {
+            setAlert({ type: 'danger', message: 'You must re-enter the correct confirmation password' })
+            setTimeout(() => setAlert(null), 5000)
         }
         else {
             try {
                 const userResgisterData = await registerUser(RegisterForm)
-                if(userResgisterData.success){
-                    setAlert({type: 'success', message: 'Account created successfully!'})
-                    setTimeout(()=> setAlert(null), 5000)
+                if (userResgisterData.success) {
+                    setAlert({ type: 'success', message: 'Account created successfully!' })
+                    setTimeout(() => setAlert(null), 5000)
                 }
-                else{
-                    setAlert({type: 'danger', message: userResgisterData.message})
-                    setTimeout(()=> setAlert(null), 5000)
+                else {
+                    setAlert({ type: 'danger', message: userResgisterData.message })
+                    setTimeout(() => setAlert(null), 5000)
                 }
             }
-            catch (error){
+            catch (error) {
                 console.log(error)
             }
         }
@@ -55,83 +87,83 @@ const UserLoginForm =() =>{
 
     body = (
         <>
-        <div className="grid login-grid main login-box">
-            <div className='ute-title'>
-                <h3>Sign up to continue!</h3>
-                <div className='login-social'></div>
-                <span className='ute-login-sml-text'>Please enter your information</span>
+            <div className="grid login-grid main login-box">
+                <div className='ute-title'>
+                    <h3>Sign up to continue!</h3>
+                    <div className='login-social'></div>
+                    <span className='ute-login-sml-text'>Please enter your information</span>
+                </div>
+                <Form className='my-4' id='form-login' onSubmit={userRegister}>
+                    <AlertMessage info={alert} />
+                    <Form.Group>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder=''
+                            name='username'
+                            value={username}
+                            required
+                            onChange={onChangeRegisterForm}
+
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type='text'
+                            placeholder=''
+                            name='email'
+                            value={email}
+                            required
+                            onChange={onChangeRegisterForm}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            placeholder=''
+                            name='password'
+                            value={password}
+                            required
+                            onChange={onChangeRegisterForm}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                            type='password'
+                            placeholder=''
+                            name='confirmpassword'
+                            value={confirmpassword}
+                            required
+                            onChange={onChangeRegisterForm}
+                        />
+                    </Form.Group>
+                    {acceptOtp}
+                </Form>
+                <p> Aready have an account?
+                    <Link to='/login'>
+                        login
+                    </Link>
+                </p>
+
             </div>
-            <Form className='my-4' id='form-login' onSubmit={userRegister}>
-            <AlertMessage info={alert}/>
-                <Form.Group>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control 
-                        type='text' 
-                        placeholder='' 
-                        name='username'
-                        value={username} 
-                        required
-                        onChange={onChangeRegisterForm} 
-                        
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control 
-                        type='text' 
-                        placeholder='' 
-                        name='email'
-                        value={email} 
-                        required 
-                        onChange={onChangeRegisterForm} 
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                        type='password' 
-                        placeholder='' 
-                        name='password'
-                        value={password} 
-                        required 
-                        onChange={onChangeRegisterForm} 
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control 
-                        type='password' 
-                        placeholder='' 
-                        name='confirmpassword'
-                        value={confirmpassword} 
-                        required 
-                        onChange={onChangeRegisterForm} 
-                    />
-                </Form.Group>
-                <Button className='mt-2' variant='primary' type='submit'>Register</Button>
-            </Form>
-            <p> Aready have an account?
-                <Link to='/login'>
-                    login
-                </Link>
-            </p>
-            
-        </div>
         </>
     )
 
     return (
         <>
-        {/*Header,logo*/}
-        <div class="utew-login-top-header">
-            <div class="topLeft">
-                <div class="topLeftTitle">
-                    <Link className='homelink' to="/">Life Stories</Link>
+            {/*Header,logo*/}
+            <div class="utew-login-top-header">
+                <div class="topLeft">
+                    <div class="topLeftTitle">
+                        <Link className='homelink' to="/">Life Stories</Link>
+                    </div>
                 </div>
             </div>
-        </div>
-        {/*Form login*/}
-        {body}
+            {/*Form login*/}
+            {body}
         </>
     )
 }
--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -3,76 +3,80 @@ import { Link } from "react-router-dom";
 import ManhImg from '../assets/manh.png'
 import QuangImg from '../assets/quang.jpg'
 import TuanAnhImg from '../assets/tuananh.png'
+import Topbar from "../components/layout/TopBar";
 
 export default function About() {
     return (
-        <div className="about">
-            <div className="aboutContainer">
-                <div className="aboutTitle">
-                    <span className="aboutTitleSm">Something about us</span>
-                    <span className="aboutTitleLg">We are Ute-ers</span>
-                    <span className="aboutTitleDesc">How do we know each other?</span>
-                </div>
-                <div className="aboutList">
-                    <div className="aboutListItem item1">
-                        <img src={QuangImg} alt="" className="aboutImg" />
-                        <p className="aboutInfo">Front-end</p>
-                        <span className="aboutName">Trần Văn Quang - 19110271</span>
-                        <div className="aboutContact">
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
-                                <Facebook className="aboutIcon"/>
-                            </Link>     
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
-                                <Instagram className="aboutIcon"/>
-                            </Link>  
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
-                                <Twitter className="aboutIcon"/>
-                            </Link>  
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
-                                <Pinterest className="aboutIcon"/>
-                            </Link>
+        <>
+            <Topbar/>
+            <div className="about">
+                <div className="aboutContainer">
+                    <div className="aboutTitle">
+                        <span className="aboutTitleSm">Something about us</span>
+                        <span className="aboutTitleLg">We are Ute-ers</span>
+                        <span className="aboutTitleDesc">How do we know each other?</span>
+                    </div>
+                    <div className="aboutList">
+                        <div className="aboutListItem item1">
+                            <img src={QuangImg} alt="" className="aboutImg" />
+                            <p className="aboutInfo">Front-end</p>
+                            <span className="aboutName">Trần Văn Quang - 19110271</span>
+                            <div className="aboutContact">
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
+                                    <Facebook className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
+                                    <Instagram className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
+                                    <Twitter className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/tomo.quang.0901" }} target="_blank" >
+                                    <Pinterest className="aboutIcon" />
+                                </Link>
+                            </div>
                         </div>
-                </div>
-                <div className="aboutListItem item2">
-                    <img src={ManhImg } alt="" className="aboutImg" />
-                        <p className="aboutInfo">Back-end</p>
-                        <span className="aboutName">Lê Nguyễn Đức Mạnh - 19110240</span>
-                        <div className="aboutContact">
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
-                                <Facebook className="aboutIcon"/>
-                            </Link>     
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
-                                <Instagram className="aboutIcon"/>
-                            </Link>  
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
-                                <Twitter className="aboutIcon"/>
-                            </Link>  
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
-                                <Pinterest className="aboutIcon"/>
-                            </Link>
+                        <div className="aboutListItem item2">
+                            <img src={ManhImg} alt="" className="aboutImg" />
+                            <p className="aboutInfo">Back-end</p>
+                            <span className="aboutName">Lê Nguyễn Đức Mạnh - 19110240</span>
+                            <div className="aboutContact">
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
+                                    <Facebook className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
+                                    <Instagram className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
+                                    <Twitter className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/LeStark.manh05" }} target="_blank" >
+                                    <Pinterest className="aboutIcon" />
+                                </Link>
+                            </div>
                         </div>
-                </div>
-                <div className="aboutListItem item3">
-                    <img src={TuanAnhImg} alt="" className="aboutImg" />
-                        <p className="aboutInfo">Back-end</p>
-                        <span className="aboutName">Ngô Bùi Tuấn Anh - 19110163</span>
-                        <div className="aboutContact">
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
-                                <Facebook className="aboutIcon"/>
-                            </Link>     
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
-                                <Instagram className="aboutIcon"/>
-                            </Link>  
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
-                                <Twitter className="aboutIcon"/>
-                            </Link>  
-                            <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
-                                <Pinterest className="aboutIcon"/>
-                            </Link>
+                        <div className="aboutListItem item3">
+                            <img src={TuanAnhImg} alt="" className="aboutImg" />
+                            <p className="aboutInfo">Back-end</p>
+                            <span className="aboutName">Ngô Bùi Tuấn Anh - 19110163</span>
+                            <div className="aboutContact">
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
+                                    <Facebook className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
+                                    <Instagram className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
+                                    <Twitter className="aboutIcon" />
+                                </Link>
+                                <Link className="link" to={{ pathname: "https://www.facebook.com/Kabuto.Hido" }} target="_blank" >
+                                    <Pinterest className="aboutIcon" />
+                                </Link>
+                            </div>
                         </div>
-                </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
